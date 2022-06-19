f03bd4dbf5b4d5ea63dfe9f03eb9e796d852d130
Robert Petrosian <robertp@mail.ru>
2022-06-19 09:35:39 +0300

    1) добавлено ТЗ №2 к ДЗ урока 13 2) изменена программа файл-менеджер для чтения/записи в файл 3) дописан тест test_filemanager.py для модуля ctrl_cwd.py

--- test_filemanager.py
+++ test_filemanager.py
@@ -35,6 +35,7 @@ def test_cr_file():
     assert ctrl_cwd.cr_file(new_file) == '' # ile has been created succesfully
     assert os.path.exists(new_file) # file exists
     os.remove(new_file) # clean up
+    assert not os.path.exists(new_file) # file exists
 
 def test_del_file():
     new_folder = 'folder-'+str(random.randint(1,1000)) #random name of folder
@@ -56,6 +57,7 @@ def test_cp_file():
     assert ctrl_cwd.cp_file(new_file_from, new_file_to) == ''  # file has been rewritten
     assert os.path.exists(new_file_to) #  file exists
     os.remove(new_file_to) # clean up
+    os.remove(new_file_from) # clean up
 
 def test_show_folders():
     for item in ctrl_cwd.show_folders():
@@ -64,6 +66,31 @@ def test_show_folders():
 def test_show_files():
     for item in ctrl_cwd.show_files():
         assert os.path.isfile(item)
+
+def test_save_cwd_to_file():
+    '''проверка 3х функций создание, проверка и удаление каталога, чтобы сама проверка была чистой функцией'''
+    cur_folder = os.getcwd() # запоминаем текущий каталог
+    new_folder = 'folder-'+str(random.randint(1,1000)) #random name of folder
+    os.mkdir(new_folder)
+    os.chdir(new_folder)
+    os.mkdir(new_folder)
+
+    with open('newfile.txt','w') as f:
+        f.write('')
+
+    ctrl_cwd.save_cwd_to_file()
+    with open('listdir.txt') as f:
+        lines = f.readlines()
+
+    assert len(lines) == 2 # numbers of lines in file
+    assert lines[0] == 'Files: newfile.txt\n' # first line
+    assert lines[1] == 'Dirs: '+new_folder  # second line
+    os.rmdir(new_folder)  # dir new_folder has been deleted
+    os.remove('newfile.txt')
+    os.remove('listdir.txt')
+    os.chdir('..')
+    os.rmdir(new_folder)
+
 
 if __name__ == '__main__':
     test_show_files()
